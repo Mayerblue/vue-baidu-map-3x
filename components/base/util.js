cbaa2d9cffa8285c134f2499d97914ec2e76ca9a
“keliangmei” <keliangmei@syxysoft.com>
2025-03-26 14:15:03 +0800

    feat: 代理地址的设置

--- components/base/util.js
+++ components/base/util.js
@@ -1,13 +1,13 @@
 import { createPoint } from './factory'
 
 export const isPoint = obj => obj.lng && obj.lat
-export const checkType = val => Object.prototype.toString.call(val).slice(8, -1)
+export const checkType = val => Object.prototype.toString.call(val).slice(8,-1)
 
-export const getPosition = (BMap, point) => isPoint(point) ? createPoint(BMap, point) : point
+export const getPosition = (BMap,point) => isPoint(point) ? createPoint(BMap,point) : point
 
 export const deleteEmptyKey = (obj) => {
     Object.keys(obj).forEach(key => {
-        if (typeof obj[key] === 'undefined') {
+        if(typeof obj[key] === 'undefined') {
             delete obj[key];
         }
     });
@@ -15,13 +15,16 @@ export const deleteEmptyKey = (obj) => {
 
 let config = {
     v: '3.0',
-    type: 'API'
+    type: 'API',
+    baseUrl: '/_BMapService',
 };
 
 export const setConfig = (options = {}) => {
-    config = Object.assign(config, options);
-    if (!config.ak) {
+    config = Object.assign(config,options);
+    if(!config.ak) {
         console.error('请配置ak：百度地图开发者平台申请的密钥');
+    } else if(!config.baseUrl) {
+        console.error('请配置代理地址！');
     }
 };
 
